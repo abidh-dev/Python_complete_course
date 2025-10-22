15d3737485c57c4ef40a2c8b350c3a0242fc9054
Engineer AbidH <abidali70sb@gmail.com>
2025-10-22 18:01:49 +0200

    Strings intro and Methods

---
+++ Chapter1/Chapter3/03_str_functions.py
@@ -0,0 +1,10 @@
+name = "Andrew"
+print(name.upper())      # Convert all characters to uppercase
+print(name.lower())      # Convert all characters to lowercase
+print(name.capitalize()) # Capitalize the first character
+print(name.title())      # Capitalize the first character of each word
+print(name.swapcase())   # Swap the case of each character
+print(name.count('a'))   # Count occurrences of 'a'
+print(name.find('d'))    # Find the index of first occurrence of 'd'
+print(name.replace('A', 'E'))  # Replace 'A' with 'E'
+print(name.split('e'))  # Split the string at each 'e'
