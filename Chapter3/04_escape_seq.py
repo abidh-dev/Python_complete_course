4d9e5075c9ebdb4eece2c4522d9bfcec3e2cc19b
Engineer AbidH <abidali70sb@gmail.com>
2025-11-02 14:45:28 +0100

    escape sequence

---
+++ Chapter3/04_escape_seq.py
@@ -0,0 +1,20 @@
+# |n is an escape sequence that adds a new line
+# Example:
+var = "abid is a good boy\nhe is learning python"
+print(var)
+
+# |t is an escape sequence that adds a tab space
+# Example:
+var2 = "abid is a good boy\the is learning python"
+print(var2)
+
+# |\' is an escape sequence that allows you to include a single quote in a string
+# Example:
+var3 = 'abid\'s book'
+print(var3)
+
+# |\" is an escape sequence that allows you to include a double quote in a string
+# Example:
+var4 = "He said, \"Python is awesome!\""
+print(var4)
+
